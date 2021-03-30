834037f83f38ed5c533c3ab6fae5b4d4c9c1afb8
sr7sidharth <45019932+sr7sidharth@users.noreply.github.com>
2021-03-29 18:39:11 -0700

    set up directory structure

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 #contains the main function to run the bot
+import discord, os
+from dotenv import load_dotenv
+import mysql.connector
+
+if __name__ == "__main__":
+    # Get bot token and database credentials
+    load_dotenv()
+    token = os.getenv("discord_token")
+    username = os.getenv("user")
+    password = os.getenv("password")
+    database_name = os.getenv("database_name")
+
+    # Connect to database
+    db = mysql.connector.connect(
+        host = "localhost",
+        user = username,
+        password = password,
+        database = database_name
+    )
+
+    cursor = db.cursor()
+
+    # Activate bot
+    client = Discord.Client()
+    client.run(token)
